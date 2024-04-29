43c2c147f9057564bcd19b4d634af9c0c821aff3
schadalawada <schadalawada@equinix.com>
2024-04-29 11:29:49 -0700

    feat: onboarding NGINX module

--- examples/Palo-Alto-Network-Firewall/pa-vm-firewall-ha/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "device_details" {
-  description = "Virtual device details"
-  value       = module.pa_vm_ha
-}
--- examples/Palo-Alto-Network-Firewall/pa-vm-firewall-single/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    equinix = {
-      source  = "equinix/equinix"
-      version = "~> 1.34.0"
-    }
-  }
-}
